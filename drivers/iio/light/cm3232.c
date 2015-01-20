f3bbb8bdb39e33c1adb9ba129cb59c3c2a8146ca
Lu Baolu <baolu.lu@linux.intel.com>
2015-01-20 15:22:27 +0800

    iio: cm3232: v2: Add cm3232 ALS driver
    CM3232 is an advanced ambient light sensor with
    I2C protocol interface. The I2C slave address is
    internally hardwired as 0x10 (7-bit).  Writing
    to configure register is byte mode, but reading
    ALS register requests to use word mode for 16-bit
    resolution.
    
    This patch is the latest driver from the vendor
    and replaces the old one.
    This version of the driver includes code to
    retrieve sensitivity scale value through ACPI interface.
    
    Tracked-On: https://jira01.devtools.intel.com/browse/GMINL-6139
    
    Change-Id: I643522639e9fcc202856dba42c01999cbccc5a22
    Signed-off-by: Kevin Tsai <ktsai@capellamicro.com>
    Signed-off-by: Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>
    Signed-off-by: Lu Baolu <baolu.lu@linux.intel.com>
    Signed-off-by: Irina Patru <irina.patru@intel.com>

--- drivers/iio/light/cm3232.c
+++ drivers/iio/light/cm3232.c
@@ -1,146 +1,233 @@
 /*
- * Copyright (C) 2014 Capella Microsystems Inc.
+ * CM3232 Ambient Light Sensor
+ *
+ * Copyright (C) 2014-2015 Capella Microsystems Inc.
  * Author: Kevin Tsai <ktsai@capellamicro.com>
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2, as published
  * by the Free Software Foundation.
  *
+ * IIO driver for CM3232 (7-bit I2C slave address 0x10).
+ *
  */
 
-#include <linux/delay.h>
-#include <linux/err.h>
 #include <linux/i2c.h>
-#include <linux/mutex.h>
 #include <linux/module.h>
-#include <linux/interrupt.h>
-#include <linux/regulator/consumer.h>
 #include <linux/iio/iio.h>
 #include <linux/iio/sysfs.h>
-#include <linux/iio/events.h>
 #include <linux/init.h>
+
+#ifdef CONFIG_ACPI
 #include <linux/acpi.h>
+#endif /* CONFIG_ACPI */
 
 /* Registers Address */
-#define CM3232_REG_ADDR_CMD		0x00
-#define CM3232_REG_ADDR_ALS		0x50
-#define CM3232_REG_ADDR_ID		0x53
+#define CM3232_REG_ADDR_CMD		   0x00
+#define CM3232_REG_ADDR_ALS		   0x50
+#define CM3232_REG_ADDR_ID	   0x53
 
-/* CMD register */
-#define CM3232_CMD_ALS_DISABLE		BIT(0)
-#define	CM3232_CMD_ALS_HS		BIT(1)
+#define CM3232_CMD_ALS_DISABLE	   BIT(0)
 
-#define CM3232_CMD_ALS_IT_SHIFT         2
-#define CM3232_CMD_ALS_IT_MASK          (0x07 << CM3232_CMD_ALS_IT_SHIFT)
-#define CM3232_CMD_ALS_IT_DEFAULT       (0x01 << CM3232_CMD_ALS_IT_SHIFT)
+#define CM3232_CMD_ALS_IT_SHIFT		   2
+#define CM3232_CMD_ALS_IT_MASK	   (BIT(2) | BIT(3) | BIT(4))
+#define CM3232_CMD_ALS_IT_DEFAULT  (0x01 << CM3232_CMD_ALS_IT_SHIFT)
 
-#define	CM3232_CMD_ALS_RESET		BIT(6)
+#define CM3232_CMD_ALS_RESET	   BIT(6)
 
-#define CM3232_CMD_DEFAULT		CM3232_CMD_ALS_IT_DEFAULT
+#define CM3232_CMD_DEFAULT	   CM3232_CMD_ALS_IT_DEFAULT
 
-#define CM3232_CALIBSCALE_DEFAULT	100000
-#define CM3232_CALIBSCALE_RESOLUTION	100000
-#define CM3232_MLUX_PER_LUX		1000
+#define CM3232_HW_ID		   0x32
+#define CM3232_CALIBSCALE_DEFAULT  100000
+#define CM3232_CALIBSCALE_RESOLUTION   100000
+#define CM3232_MLUX_PER_LUX		   1000
 
-#define CM3232_MLUX_PER_BIT_DEFAULT	64
-#define CM3232_MLUX_PER_BIT_BASE_IT	100000
-static const int CM3232_als_it_bits[] = { 0, 1, 2, 3, 4, 5};
-static const int CM3232_als_it_values[] = {
-			100000, 200000, 400000, 800000, 1600000, 3200000};
+#define CM3232_MLUX_PER_BIT_DEFAULT    64
+#define CM3232_MLUX_PER_BIT_BASE_IT    100000
+
+static const struct {
+	int val;
+	int val2;
+	u8 it;
+} cm3232_als_it_scales[] = {
+	{0, 100000, 0}, /* 0.100000 */
+	{0, 200000, 1}, /* 0.200000 */
+	{0, 400000, 2}, /* 0.400000 */
+	{0, 800000, 3}, /* 0.800000 */
+	{1, 600000, 4}, /* 1.600000 */
+	{3, 200000, 5}, /* 3.200000 */
+};
 
 struct cm3232_als_info {
-	u32 id;
+	u8 regs_cmd_default;
+	u8 hw_id;
 	int calibscale;
 	int mlux_per_bit;
 	int mlux_per_bit_base_it;
-	const int *als_it_bits;
-	const int *als_it_values;
-	const int num_als_it;
-	int als_raw;
 };
 
 static struct cm3232_als_info cm3232_als_info_default = {
-	.id = 3232,
+	.regs_cmd_default = CM3232_CMD_DEFAULT,
+	.hw_id = CM3232_HW_ID,
 	.calibscale = CM3232_CALIBSCALE_DEFAULT,
 	.mlux_per_bit = CM3232_MLUX_PER_BIT_DEFAULT,
 	.mlux_per_bit_base_it = CM3232_MLUX_PER_BIT_BASE_IT,
-	.als_it_bits = CM3232_als_it_bits,
-	.als_it_values = CM3232_als_it_values,
-	.num_als_it = ARRAY_SIZE(CM3232_als_it_bits),
 };
 
 struct cm3232_chip {
 	struct i2c_client *client;
-	struct mutex lock;
 	struct cm3232_als_info *als_info;
 	u8 regs_cmd;
+	u16 regs_als;
 };
 
 static int cm3232_get_lux(struct cm3232_chip *chip);
-static int cm3232_read_als_it(struct cm3232_chip *chip, int *val2);
+static int cm3232_read_als_it(struct cm3232_chip *chip, int *val, int *val2);
+
+
+#ifdef CONFIG_ACPI
+/**
+ * cm32181_acpi_get_cpm_info() - Get CPM object from ACPI
+ * @client pointer of struct i2c_client.
+ * @obj_name   pointer of ACPI object name.
+ * @count  maximum size of return array.
+ * @vals   pointer of array for return elements.
+ *
+ * Convert ACPI CPM table to array. Special thanks Srinivas Pandruvada's
+ * help to implement this routine.
+ *
+ * Return: -ENODEV for fail.  Otherwise is number of elements.
+ */
+static int cm32181_acpi_get_cpm_info(struct i2c_client *client, char *obj_name,
+		int count, u64 *vals)
+{
+	acpi_handle handle;
+	struct acpi_buffer buffer = {ACPI_ALLOCATE_BUFFER, NULL};
+	int i;
+	acpi_status status;
+	union acpi_object *cpm;
+
+	handle = ACPI_HANDLE(&client->dev);
+	if (!handle)
+		return -ENODEV;
+
+	status = acpi_evaluate_object(handle, obj_name, NULL, &buffer);
+	if (ACPI_FAILURE(status)) {
+		dev_err(&client->dev, "object %s not found\n", obj_name);
+		return -ENODEV;
+	}
+
+	cpm = buffer.pointer;
+	for (i = 0; i < cpm->package.count && i < count; ++i) {
+		union acpi_object *elem;
+
+		elem = &(cpm->package.elements[i]);
+		vals[i] = elem->integer.value;
+	}
+
+	kfree(buffer.pointer);
+
+	return cpm->package.count;
+}
+#endif /* CONFIG */
 
 /**
- * cm3232_reg_init() - Initialize CM3232 registers
- * @chip:	pointer of struct cm3232.
+ * cm3232_reg_init() - Initialize CM3232
+ * @chip:  pointer of struct cm3232_chip.
  *
- * Initialize CM3232 ambient light sensor register to default values.
+ * Check and initialize CM3232 ambient light sensor.
  *
-  Return: 0 for success; otherwise for error code.
+ * Return: 0 for success; otherwise for error code.
  */
 static int cm3232_reg_init(struct cm3232_chip *chip)
 {
 	struct i2c_client *client = chip->client;
-	struct cm3232_als_info *als_info;
 	s32 ret;
+#ifdef CONFIG_ACPI
+	int cpm_elem_count;
+	u64 cpm_elems[20];
+#endif /*CONFIG_ACPI */
+
+	chip->als_info = &cm3232_als_info_default;
+
+#ifdef CONFIG_ACPI
+	if (ACPI_HANDLE(&client->dev)) {
+		/* Load from ACPI */
+		cpm_elem_count = cm32181_acpi_get_cpm_info(client, "CPM0",
+				ARRAY_SIZE(cpm_elems),
+				cpm_elems);
+		if (cpm_elem_count > 0) {
+			int header_num = 3;
+			int regs_bmp = cpm_elems[2];
+
+			chip->als_info->hw_id = (u8)cpm_elems[0];
+			if (regs_bmp & BIT(0))
+				chip->als_info->regs_cmd_default =
+					cpm_elems[header_num];
+		}
+
+		cpm_elem_count = cm32181_acpi_get_cpm_info(client, "CPM1",
+				ARRAY_SIZE(cpm_elems),
+				cpm_elems);
+		if (cpm_elem_count > 0) {
+			chip->als_info->mlux_per_bit = (int)cpm_elems[0] / 100;
+			chip->als_info->calibscale = (int)cpm_elems[1];
+		}
+	}
+#endif /* CONFIG_ACPI */
 
 	/* Identify device */
 	ret = i2c_smbus_read_word_data(client, CM3232_REG_ADDR_ID);
-	if (ret < 0)
+	if (ret < 0) {
+		dev_err(&chip->client->dev, "Error reading addr_id\n");
 		return ret;
-	if ((ret & 0xFF) != 0x32)
+	}
+
+	if ((ret & 0xFF) != chip->als_info->hw_id)
 		return -ENODEV;
 
 	/* Disable and reset device */
 	chip->regs_cmd = CM3232_CMD_ALS_DISABLE | CM3232_CMD_ALS_RESET;
 	ret = i2c_smbus_write_byte_data(client, CM3232_REG_ADDR_CMD,
-				chip->regs_cmd);
-	if (ret < 0)
+			chip->regs_cmd);
+	if (ret < 0) {
+		dev_err(&chip->client->dev, "Error writing reg_cmd\n");
 		return ret;
-
-	/* Initialization */
-	als_info = chip->als_info = &cm3232_als_info_default;
-	chip->regs_cmd = CM3232_CMD_DEFAULT;
+	}
+	/* Register default value */
+	chip->regs_cmd = chip->als_info->regs_cmd_default;
 
 	/* Configure register */
 	ret = i2c_smbus_write_byte_data(client, CM3232_REG_ADDR_CMD,
-				chip->regs_cmd);
+			chip->regs_cmd);
 	if (ret < 0)
-		return ret;
+		dev_err(&chip->client->dev, "Error writing reg_cmd\n");
 
 	return 0;
 }
 
 /**
- *  cm3232_read_als_it() - Get sensor integration time (ms)
- *  @chip:	pointer of struct cm3232
- *  @val2:	pointer of int to load the als_it value.
+ *	cm3232_read_als_it() - Get sensor integration time
+ *	@chip: pointer of struct cm3232_chip
+ *	@val:  pointer of int to load the integration (sec).
+ *	@val2: pointer of int to load the integration time (microsecond).
  *
- *  Report the current integration time in milliseconds.
+ *	Report the current integration time.
  *
- *  Return: IIO_VAL_INT_PLUS_MICRO for success, otherwise -EINVAL.
+ *	Return: IIO_VAL_INT_PLUS_MICRO for success, otherwise -EINVAL.
  */
-static int cm3232_read_als_it(struct cm3232_chip *chip, int *val2)
+static int cm3232_read_als_it(struct cm3232_chip *chip, int *val, int *val2)
 {
-	struct cm3232_als_info *als_info = chip->als_info;
 	u16 als_it;
 	int i;
 
 	als_it = chip->regs_cmd;
 	als_it &= CM3232_CMD_ALS_IT_MASK;
 	als_it >>= CM3232_CMD_ALS_IT_SHIFT;
-	for (i = 0; i < als_info->num_als_it; i++) {
-		if (als_it == als_info->als_it_bits[i]) {
-			*val2 = als_info->als_it_values[i];
+	for (i = 0; i < ARRAY_SIZE(cm3232_als_it_scales); i++) {
+		if (als_it == cm3232_als_it_scales[i].it) {
+			*val = cm3232_als_it_scales[i].val;
+			*val2 = cm3232_als_it_scales[i].val2;
 			return IIO_VAL_INT_PLUS_MICRO;
 		}
 	}
@@ -150,85 +237,89 @@ static int cm3232_read_als_it(struct cm3232_chip *chip, int *val2)
 
 /**
  * cm3232_write_als_it() - Write sensor integration time
- * @chip:	pointer of struct cm3232.
- * @val:	integration time in milliseconds.
+ * @chip:  pointer of struct cm3232_chip.
+ * @val:   integration time in second.
+ * @val2:  integration time in microsecond.
  *
- * Convert integration time (ms) to sensor value.
+ * Convert integration time to sensor value.
  *
- * Return: i2c_smbus_write_word_data command return value.
+ * Return: i2c_smbus_write_byte_data command return value.
  */
-static int cm3232_write_als_it(struct cm3232_chip *chip, int val)
+static int cm3232_write_als_it(struct cm3232_chip *chip, int val, int val2)
 {
 	struct i2c_client *client = chip->client;
-	struct cm3232_als_info *als_info = chip->als_info;
-	u16 als_it;
-	int ret, i;
+	u16 als_it, cmd;
+	int i;
+	s32 ret;
 
-	for (i = 0; i < als_info->num_als_it; i++)
-		if (val <= als_info->als_it_values[i])
-			break;
-	if (i >= als_info->num_als_it)
-		i = als_info->num_als_it - 1;
+	for (i = 0; i < ARRAY_SIZE(cm3232_als_it_scales); i++) {
+		if (val == cm3232_als_it_scales[i].val &&
+				val2 == cm3232_als_it_scales[i].val2) {
 
-	als_it = als_info->als_it_bits[i];
-	als_it <<= CM3232_CMD_ALS_IT_SHIFT;
+			als_it = cm3232_als_it_scales[i].it;
+			als_it <<= CM3232_CMD_ALS_IT_SHIFT;
 
-	mutex_lock(&chip->lock);
-	chip->regs_cmd &= ~CM3232_CMD_ALS_IT_MASK;
-	chip->regs_cmd |= als_it;
-	ret = i2c_smbus_write_byte_data(client, CM3232_REG_ADDR_CMD,
-			chip->regs_cmd);
-	mutex_unlock(&chip->lock);
-
-	return ret;
+			cmd = chip->regs_cmd & ~CM3232_CMD_ALS_IT_MASK;
+			cmd |= als_it;
+			ret = i2c_smbus_write_byte_data(client,
+					CM3232_REG_ADDR_CMD,
+					cmd);
+			if (ret < 0)
+				return ret;
+			chip->regs_cmd = cmd;
+			return 0;
+		}
+	}
+	return -EINVAL;
 }
 
 /**
  * cm3232_get_lux() - report current lux value
- * @chip:	pointer of struct cm3232.
+ * @chip:  pointer of struct cm3232_chip.
  *
- * Convert sensor raw data to lux.  It depends on integration
+ * Convert sensor data to lux.	It depends on integration
  * time and calibscale variable.
  *
- * Return: Positive value is lux, otherwise is error code.
+ * Return: Zero or positive value is lux, otherwise error code.
  */
 static int cm3232_get_lux(struct cm3232_chip *chip)
 {
 	struct i2c_client *client = chip->client;
 	struct cm3232_als_info *als_info = chip->als_info;
 	int ret;
+	int val, val2;
 	int als_it;
-	u64 tmp;
+	u64 lux;
 
 	/* Calculate mlux per bit based on als_it */
-	ret = cm3232_read_als_it(chip, &als_it);
+	ret = cm3232_read_als_it(chip, &val, &val2);
 	if (ret < 0)
 		return -EINVAL;
-	tmp = (__force u64)als_info->mlux_per_bit;
-	tmp *= als_info->mlux_per_bit_base_it;
-	tmp = div_u64 (tmp, als_it);
+	als_it = val * 1000000 + val2;
+	lux = (__force u64)als_info->mlux_per_bit;
+	lux *= als_info->mlux_per_bit_base_it;
+	lux = div_u64(lux, als_it);
+	ret = i2c_smbus_read_word_data(client, CM3232_REG_ADDR_ALS);
+	if (ret < 0) {
+		dev_err(&client->dev, "Error reading reg_addr_als\n");
+		return ret;
+	}
 
-	/* Get als_raw */
-	als_info->als_raw = i2c_smbus_read_word_data(
-				client,
-				CM3232_REG_ADDR_ALS);
-	if (als_info->als_raw < 0)
-		return als_info->als_raw;
+	chip->regs_als = (u16)ret;
+	lux *= chip->regs_als;
+	lux *= als_info->calibscale;
+	lux = div_u64(lux, CM3232_CALIBSCALE_RESOLUTION);
+	lux = div_u64(lux, CM3232_MLUX_PER_LUX);
 
-	tmp *= als_info->als_raw;
-	tmp *= als_info->calibscale;
-	tmp = div_u64(tmp, CM3232_CALIBSCALE_RESOLUTION);
-	tmp = div_u64(tmp, CM3232_MLUX_PER_LUX);
+	if (lux > 0xFFFF)
+		lux = 0xFFFF;
 
-	if (tmp > 0xFFFF)
-		tmp = 0xFFFF;
-
-	return (int)tmp;
+	return (int)lux;
 }
 
 static int cm3232_read_raw(struct iio_dev *indio_dev,
-			struct iio_chan_spec const *chan,
-			int *val, int *val2, long mask)
+		struct iio_chan_spec const *chan,
+		int *val, int *val2, long mask)
 {
 	struct cm3232_chip *chip = iio_priv(indio_dev);
 	struct cm3232_als_info *als_info = chip->als_info;
@@ -245,29 +336,34 @@ static int cm3232_read_raw(struct iio_dev *indio_dev,
 		*val = als_info->calibscale;
 		return IIO_VAL_INT;
 	case IIO_CHAN_INFO_INT_TIME:
-		*val = 0;
-		ret = cm3232_read_als_it(chip, val2);
-		return ret;
+		return cm3232_read_als_it(chip, val, val2);
+	case IIO_CHAN_INFO_RAW:
+		ret = i2c_smbus_read_word_data(chip->client,
+				CM3232_REG_ADDR_ALS);
+		if (ret < 0) {
+			dev_err(&chip->client->dev,
+					"Error reading reg_addr_als\n");
+			return ret;
+		}
+		*val = ret;
+		return IIO_VAL_INT;
 	}
 
 	return -EINVAL;
 }
-
 static int cm3232_write_raw(struct iio_dev *indio_dev,
-			struct iio_chan_spec const *chan,
-			int val, int val2, long mask)
+		struct iio_chan_spec const *chan,
+		int val, int val2, long mask)
 {
 	struct cm3232_chip *chip = iio_priv(indio_dev);
 	struct cm3232_als_info *als_info = chip->als_info;
-	long ms;
 
 	switch (mask) {
 	case IIO_CHAN_INFO_CALIBSCALE:
 		als_info->calibscale = val;
-		return val;
+		return 0;
 	case IIO_CHAN_INFO_INT_TIME:
-		ms = val * 1000000 + val2;
-		return cm3232_write_als_it(chip, (int)ms);
+		return cm3232_write_als_it(chip, val, val2);
 	}
 
 	return -EINVAL;
@@ -275,25 +371,23 @@ static int cm3232_write_raw(struct iio_dev *indio_dev,
 
 /**
  * cm3232_get_it_available() - Get available ALS IT value
- * @dev:	pointer of struct device.
- * @attr:	pointer of struct device_attribute.
- * @buf:	pointer of return string buffer.
+ * @dev:   pointer of struct device.
+ * @attr:  pointer of struct device_attribute.
+ * @buf:   pointer of return string buffer.
  *
- * Display the available integration time in milliseconds.
+ * Display the available integration time in second.
  *
  * Return: string length.
  */
 static ssize_t cm3232_get_it_available(struct device *dev,
-			struct device_attribute *attr, char *buf)
+		struct device_attribute *attr, char *buf)
 {
-	struct cm3232_chip *chip = iio_priv(dev_to_iio_dev(dev));
-	struct cm3232_als_info *als_info = chip->als_info;
 	int i, len;
 
-	for (i = 0, len = 0; i < als_info->num_als_it; i++)
+	for (i = 0, len = 0; i < ARRAY_SIZE(cm3232_als_it_scales); i++)
 		len += scnprintf(buf + len, PAGE_SIZE - len, "%u.%06u ",
-			als_info->als_it_values[i]/1000000,
-			als_info->als_it_values[i]%1000000);
+				cm3232_als_it_scales[i].val,
+				cm3232_als_it_scales[i].val2);
 	return len + scnprintf(buf + len, PAGE_SIZE - len, "\n");
 }
 
@@ -303,12 +397,12 @@ static const struct iio_chan_spec cm3232_channels[] = {
 		.info_mask_separate =
 			BIT(IIO_CHAN_INFO_PROCESSED) |
 			BIT(IIO_CHAN_INFO_CALIBSCALE) |
-			BIT(IIO_CHAN_INFO_INT_TIME),
+			BIT(IIO_CHAN_INFO_INT_TIME) |
+			BIT(IIO_CHAN_INFO_RAW),
 	}
 };
-
 static IIO_DEVICE_ATTR(in_illuminance_integration_time_available,
-			S_IRUGO, cm3232_get_it_available, NULL, 0);
+		S_IRUGO, cm3232_get_it_available, NULL, 0);
 
 static struct attribute *cm3232_attributes[] = {
 	&iio_dev_attr_in_illuminance_integration_time_available.dev_attr.attr,
@@ -327,23 +421,20 @@ static const struct iio_info cm3232_info = {
 };
 
 static int cm3232_probe(struct i2c_client *client,
-			const struct i2c_device_id *id)
+		const struct i2c_device_id *id)
 {
 	struct cm3232_chip *chip;
 	struct iio_dev *indio_dev;
 	int ret;
 
 	indio_dev = devm_iio_device_alloc(&client->dev, sizeof(*chip));
-	if (!indio_dev) {
-		dev_err(&client->dev, "devm_iio_device_alloc failed\n");
+	if (!indio_dev)
 		return -ENOMEM;
-	}
 
 	chip = iio_priv(indio_dev);
 	i2c_set_clientdata(client, indio_dev);
 	chip->client = client;
 
-	mutex_init(&chip->lock);
 	indio_dev->dev.parent = &client->dev;
 	indio_dev->channels = cm3232_channels;
 	indio_dev->num_channels = ARRAY_SIZE(cm3232_channels);
@@ -357,47 +448,55 @@ static int cm3232_probe(struct i2c_client *client,
 	ret = cm3232_reg_init(chip);
 	if (ret) {
 		dev_err(&client->dev,
-			"%s: register init failed\n",
-			__func__);
+				"%s: register init failed\n",
+				__func__);
 		return ret;
 	}
 
-	ret = iio_device_register(indio_dev);
-	return 0;
+	return iio_device_register(indio_dev);
 }
 
 static int cm3232_remove(struct i2c_client *client)
 {
 	struct iio_dev *indio_dev = i2c_get_clientdata(client);
 
+	i2c_smbus_write_byte_data(client, CM3232_REG_ADDR_CMD,
+			CM3232_CMD_ALS_DISABLE);
+
 	iio_device_unregister(indio_dev);
 	return 0;
 }
 
 static const struct i2c_device_id cm3232_id[] = {
-	{ "cm3232", 0},
-	{ }
+	{"cm3232", 0},
+	{}
 };
+
 MODULE_DEVICE_TABLE(i2c, cm3232_id);
 
-static const struct acpi_device_id cm3232_acpi_match[] = {
-	{"CM3232", 0},
-	{"CALS3232", 0},
-	{ },
-};
-MODULE_DEVICE_TABLE(acpi, cm3232_acpi_match);
-
 static const struct of_device_id cm3232_of_match[] = {
-	{ .compatible = "capella,cm3232" },
-	{ }
+	{.compatible = "capella,cm3232"},
+	{}
 };
+
+#ifdef CONFIG_ACPI
+static const struct acpi_device_id cm3232_acpi_match[] = {
+	{ "CPLM3232", 0},
+	{ "CALS3232", 0},
+	{},
+};
+
+MODULE_DEVICE_TABLE(acpi, cm3232_acpi_match);
+#endif /* CONFIG_ACPI */
 
 static struct i2c_driver cm3232_driver = {
 	.driver = {
 		.name	= "cm3232",
+#ifdef CONFIG_ACPI
+		.acpi_match_table = ACPI_PTR(cm3232_acpi_match),
+#endif /* CONFIG_ACPI */
 		.owner	= THIS_MODULE,
 		.of_match_table = of_match_ptr(cm3232_of_match),
-		.acpi_match_table = ACPI_PTR(cm3232_acpi_match),
 	},
 	.id_table	= cm3232_id,
 	.probe		= cm3232_probe,
