0862c0c2db015dc4abbd8fac74ed46575537a028
Qiuxu Zhuo <qiuxu.zhuo@intel.com>
2015-04-22 13:31:44 +0800

    ASoC: Intel: Add support for Malata8Low device
    The audio codec can't be correclty identified on the Malata (Low) variant,
    due to the missing DMI string id.
    
    This patch will add the missing DMI string identifier.
    
    Issue: GMINL-8469
    
    Change-Id: I2174f8d00c2b60c218b6e220de5574f698851761
    Signed-off-by: Qiuxu Zhuo <qiuxu.zhuo@intel.com>

--- sound/soc/intel/board/byt_cr_board_configs.h
+++ sound/soc/intel/board/byt_cr_board_configs.h
@@ -58,6 +58,7 @@ enum board_id_rt5640 {
 	RT5640_T100,
 	RT5640_MALATA,
 	RT5640_MALATA_A82I,
+	RT5640_MALATA_A8LOW,
 	RT5640_CHIPHD,
 };
 
@@ -149,6 +150,14 @@ static const struct dmi_system_id dmi_system_ids0[] = {
 		.matches = {
 			DMI_MATCH(DMI_BOARD_NAME, "A82i"),
 			DMI_MATCH(DMI_BOARD_VERSION, "1"),
+		},
+		.driver_data = (void *)&board_configs0[RT5640_MALATA],
+	},
+	[RT5640_MALATA_A8LOW] = {
+		.ident = "A8Low",
+		.matches = {
+			DMI_MATCH(DMI_BOARD_NAME, "MALATA8Low"),
+			DMI_MATCH(DMI_BOARD_VERSION, "2"),
 		},
 		.driver_data = (void *)&board_configs0[RT5640_MALATA],
 	},
